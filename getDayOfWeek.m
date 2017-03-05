7dfe3de0fe553b86cdeef95b8215ba965b9dff29
Ashley <ashley.deshields.2016@gmail.com>
2017-03-04 23:57:47 -0500

    mental note: 240 pushups

--- getDayOfWeek.m
+++ getDayOfWeek.m
@@ -1,10 +1,10 @@
 function dayOfWeek = getDayOfWeek(sessions)
-dayValues = {zeros(height(sessions), 1)};
-for days = sessions{:,2}
-    reday = datevec(days);
-    DayNumber = datenum(reday);  
-    dayValues{days} = DayNumber;
-    
+dayValues = zeros(height(sessions), 1);
+
+for days = 1:length(sessions{:,2})
+    reday = datevec(sessions{days,2}); %where it expands into vector
+    %DayNumber = datenum(reday);  %where it comes together as a numerical representation
+    dayValues(days) = datenum(reday); % PROBLEM CHILD
 end
 dayOfWeek = dayValues;
     
