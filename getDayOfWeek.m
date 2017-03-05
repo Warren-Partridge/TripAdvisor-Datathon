fb1111640f26de32a67ce3242cbaa84f4583fa65
Ashley <ashley.deshields.2016@gmail.com>
2017-03-04 22:01:33 -0500

    hahahaha who said this work not moi

--- getDayOfWeek.m
+++ getDayOfWeek.m
@@ -1,5 +1,5 @@
 function dayOfWeek = getDayOfWeek(sessions)
-dayValues = {zeros(height(sessions))};
+dayValues = {zeros(height(sessions), 1)};
 for days = sessions{:,2}
     reday = datevec(days);
     DayNumber = datenum(reday);  
